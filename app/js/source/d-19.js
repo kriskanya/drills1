0a1f31ea717c1874ec9e4ca34b52864af3f214e7
Kristian Kanya <kriskanya@gmail.com>
2014-04-29 00:11:13 -0500

    mon commit

--- app/js/source/d-19.js
+++ app/js/source/d-19.js
@@ -1,42 +1,42 @@
-(function(){
-  'use strict';
-
-  $(document).ready(init);
-
-    function init(){
-      $('#get').click(clickSum);
-    }
-
-    function clickSum(){
-      $('#symbols').val().split(',').map(stripUC).forEach(getQuote);
-    }
-
-    function url(symbol){
-      return 'http://dev.markitondemnad...' + symbol + '&callback=?';
-    }
-
-    function stripUC(symbol){
-      return symbol.trim().toUpperCase();    //convert array of symbols to an array of URLs
-    }
-
-    var sum = 0;
-    var count = 0;
-    function getQuote(url, index, urls){
-      $.getJSON(url, function(){
-        sum = 0;  //need to reset this to zero, if you want to run it again
-        count = 0;
-
-        sum += quote.LastPrice;
-        count++
-        if(count === urls.length){
-        $('#result').text(sum);
-        }
-      });
-    }
-
-
-// $containerDiv.append('<div class="quoteTop">' + data.Symbol + '</div>');
-
-
-
-})();
+// (function(){
+//   'use strict';
+//
+//   $(document).ready(init);
+//
+//     function init(){
+//       $('#get').click(clickSum);
+//     }
+//
+//     function clickSum(){
+//       $('#symbols').val().split(',').map(stripUC).forEach(getQuote);
+//     }
+//
+//     function url(symbol){
+//       return 'http://dev.markitondemnad...' + symbol + '&callback=?';
+//     }
+//
+//     function stripUC(symbol){
+//       return symbol.trim().toUpperCase();    //convert array of symbols to an array of URLs
+//     }
+//
+//     var sum = 0;
+//     var count = 0;
+//     function getQuote(url, index, urls){
+//       $.getJSON(url, function(){
+//         sum = 0;  //need to reset this to zero, if you want to run it again
+//         count = 0;
+//
+//         sum += quote.LastPrice;
+//         count++
+//         if(count === urls.length){
+//         $('#result').text(sum);
+//         }
+//       });
+//     }
+//
+//
+// // $containerDiv.append('<div class="quoteTop">' + data.Symbol + '</div>');
+//
+//
+//
+// })();
